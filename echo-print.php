1b8d282e6fb46de5fd93a7192af9d21ca58dd37d
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2025-02-03 01:10:42 +0800

    echo VS Print

---
+++ echo-print.php
@@ -0,0 +1,10 @@
+<?php 
+    echo "<h2> PHP is fun </h2><br>";
+    echo "Hello, PHP<br>";
+    echo "I'm about to learn from PHP.";
+
+    print "<h2> PHP is fun </h2><br>";
+    print "Hello, PHP magic<br>";
+    print "I'm about to learn from PHP.";
+    print "This"; "string"; "was"; "made"; "with"; "multiple"; "parameters";
+?>
