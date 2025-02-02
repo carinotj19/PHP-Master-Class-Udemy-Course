e83bc5f6b0886edf2639fdcd3090c872728ed9a8
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2025-02-03 07:46:54 +0800

    Data Type

---
+++ dataType.php
@@ -0,0 +1,17 @@
+<?php
+
+    $earthPopulation = 78000000;
+    $phpReleases = 8;
+    echo "<p>Earth population: $earthPopulation</p>";
+    echo "<p>PHP Releases: $phpReleases</p>";
+
+    $pi = 3.1416;
+    $price = 19.99;
+    echo "<p>Value of Pi: $pi</p>";
+    echo "<p>Product Prices: $$price</p>";
+
+    $greeting = "Hello, PHP World!";
+    $framework = "Laravel";
+    echo "<p>Greetig: $greeting</p>";
+    echo "<p>Preferred Framework: $framework</p>";
+?>
