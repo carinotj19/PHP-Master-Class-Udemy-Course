e00b408b55be70e441dc2f009ba5c941fd0026c0
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2025-02-03 01:14:59 +0800

    variables

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Wizard";
+    $age = 100;
+    echo "<p>Hello, $name! You are $age years old</p>";
+
+    $isWizard = true;
+    $magicLevel = 8.5;
+    echo "<p>Am I a wizard?" .($isWizard ? "Yes" : "No"). "</p>";
+    echo "<p>My Magic level is $magiLevel.</p>";
+
+    $greeting = "Greeting";
+    $greeting .= ", magical beings!";
+    echo "<p>$greeting</p>"
+?>
