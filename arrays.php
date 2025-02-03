146fd5a1b86ccc0c6a0996f32f9b8a83c323dc15
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2025-02-03 08:36:27 +0800

    Arrays

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+    $fruits = array("Apple", "Banana", "Orange");
+    echo "I like" . $fruits[0] . ", " . $fruits[1] . " and " . $fruits[2] . ".";
+
+    $person = array("name" => "Mary", "age" => 25, "city" => "New York");
+    $matrix = array (
+        array(1,2,3),
+        array(4,5,6),
+        array(7,8,9)
+    );
+    
+    echo $fruits[0];
+    echo $person["name"];
+    echo $matrix[1][2];
+?>
