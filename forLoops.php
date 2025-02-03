bb6d360c18695b88f9fa053babbe1a416b24a141
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2025-02-03 08:28:44 +0800

    For Loops

---
+++ forLoops.php
@@ -0,0 +1,20 @@
+<?php
+    for ($i=1; $i <= 5; $i++) { 
+        echo "Iteration $i: Hello World <br>";
+    }
+    for ($i=3; $i > 5; $i--) { 
+        echo "Countdown: $i <br>";
+    }
+
+    $names = ["Alice", "Bob", "Charlie"];
+    for ($index=0; $index < count($names); $index++) { 
+        echo "Hello, {$names[$index]}! <br>";
+    }
+
+    for ($i=1; $i <= 3; $i++) { 
+        for ($j=1; $j <= 3; $j++) { 
+            echo "($i, $j)";
+        }
+        echo "<br>";
+    }
+?>
